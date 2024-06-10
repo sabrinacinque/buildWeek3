579d37a0d5905daa024053d545a89b44acb24361
sabrinacinque <cinque.sabrina@gmail.com>
2024-06-10 11:14:00 +0200

    ssd

--- buildWeek3/src/app/app-routing.module.ts
+++ buildWeek3/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'homepage', loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- buildWeek3/src/app/app.module.ts
+++ buildWeek3/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './auth/login/login.component';
+import { NavbarComponent } from './mainComponent/navbar/navbar.component';
+import { FooterComponent } from './mainComponent/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

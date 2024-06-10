44c1f538cc0b87177a0cec278bef91ece1ac8a20
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-10 12:49:34 +0200

    Image homepage con annesso css

--- buildWeek3/src/app/app-routing.module.ts
+++ buildWeek3/src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'homepage', loadChildren: () => import('./pages/homepage/homepage.module').then(m => m.HomepageModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth/auth.module').then(m => m.AuthModule) }];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/homepage/homepage.module').then((m) => m.HomepageModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth/auth.module').then((m) => m.AuthModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

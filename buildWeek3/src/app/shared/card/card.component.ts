6efcc0135efef879a7c8253841a7edc354a44307
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-10 18:36:14 +0200

    chore: Update card component layout and styling

--- buildWeek3/src/app/shared/card/card.component.ts
+++ buildWeek3/src/app/shared/card/card.component.ts
@@ -5,20 +5,18 @@ import { iMenu } from '../../Models/i-menu';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
   @Input()
   category!: string;
   menu: iMenu[] = [];
 
-  constructor(
-    private menuSvc: MenuService
-  ) {}
+  constructor(private menuSvc: MenuService) {}
 
   ngOnInit() {
     if (this.category) {
-      this.menuSvc.getByCategory(this.category).subscribe(items => {
+      this.menuSvc.getByCategory(this.category).subscribe((items) => {
         this.menu = items;
       });
     }

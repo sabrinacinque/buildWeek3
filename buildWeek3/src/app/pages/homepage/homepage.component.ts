da78abb95426d8ed321cf4d23a97106a8c4df5f9
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-11 10:02:21 +0200

    chore: Add fade-in animation to homepage elements

--- buildWeek3/src/app/pages/homepage/homepage.component.ts
+++ buildWeek3/src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChildren,
+  viewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.scss'
+  styleUrls: ['./homepage.component.scss'],
 })
-export class HomepageComponent {
+export class HomepageComponent implements AfterViewInit {
+  @ViewChildren('animatedElementRight')
+  animatedElementsRight!: QueryList<ElementRef>;
+  @ViewChildren('animatedElementLeft')
+  animatedElementsLeft!: QueryList<ElementRef>;
+  @ViewChildren('animatedElementRotate')
+  animatedElementsRotate!: QueryList<ElementRef>;
 
+  ngAfterViewInit() {
+    this.animatedElementsRight.forEach((elementRef) => {
+      this.observeElement(elementRef.nativeElement, 'fade-in-right');
+    });
+
+    this.animatedElementsLeft.forEach((elementRef) => {
+      this.observeElement(elementRef.nativeElement, 'fade-in-left');
+    });
+    this.animatedElementsRotate.forEach((elementRef) => {
+      this.observeElement(elementRef.nativeElement, 'img-sushilight');
+    });
+  }
+
+  observeElement(element: Element, animationClass: string) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log('Element is intersecting:', entry.target);
+          entry.target.classList.add(animationClass);
+          observer.unobserve(entry.target);
+        }
+      });
+    });
+
+    observer.observe(element);
+  }
 }

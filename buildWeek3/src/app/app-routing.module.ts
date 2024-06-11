dd8030e4c0258770c72a0dbc11926628d98adb9b
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-11 10:28:28 +0200

    chore: Update routing configuration for lazy loading modules

--- buildWeek3/src/app/app-routing.module.ts
+++ buildWeek3/src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { MenuComponent } from './pages/menu/menu.component';
-
 
 const routes: Routes = [
-  { path: 'homepage', component: HomepageComponent },
-  { path: 'menu', loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'homepage', pathMatch: 'full', component: HomepageComponent },
+  {
+    path: 'menu',
+    loadChildren: () =>
+      import('./pages/menu/menu.module').then((m) => m.MenuModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
   { path: '**', redirectTo: '/homepage' },
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- buildWeek3/src/app/auth/auth.module.ts
+++ buildWeek3/src/app/auth/auth.module.ts
@@ -2,20 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from '../app-routing.module';
 
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { CommonModule } from '@angular/common';
 
-
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    AppRoutingModule
-  ]
+  declarations: [AuthComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}

45670f566f3953cd6472138456b8f3a82ae3aa51
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-11 18:07:31 +0200

    chore: Update navbar component to display admin link only if user is not logged in

--- buildWeek3/src/app/mainComponent/navbar/navbar.component.ts
+++ buildWeek3/src/app/mainComponent/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../auth/auth-service.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  isLogged: boolean = false;
 
+  constructor(private authSvc: AuthService) {}
+
+  ngOnInit() {
+    this.authSvc.logged$.subscribe((isLogged) => (this.isLogged = isLogged));
+  }
 }

3903e7ae7a2ecb95521d7f8ca96c61ac2574d523
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-06-12 13:06:50 +0200

    refactor: Update dashboard component to use ngbAccordion for collapsible order display

--- buildWeek3/src/app/pages/dashboard/cucina/cucina.component.ts
+++ buildWeek3/src/app/pages/dashboard/cucina/cucina.component.ts
@@ -13,6 +13,10 @@ export class CucinaComponent {
   show: boolean = false;
   orders: iOrder[] = [];
 
+  dynamicStyles = {
+    'background-color': '#99735e',
+  };
+
   constructor(private CartSvc: CartService) {}
 
   ngOnInit() {

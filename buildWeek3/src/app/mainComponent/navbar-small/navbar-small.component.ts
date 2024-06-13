0b303567706651cdb0bec8e21238d73862c7e396
BeatriceV96 <beatricevanvitelli1@gmail.com>
2024-06-13 15:32:40 +0200

    12o commit

--- buildWeek3/src/app/mainComponent/navbar-small/navbar-small.component.ts
+++ buildWeek3/src/app/mainComponent/navbar-small/navbar-small.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './../../auth/auth-service.service';
 
 @Component({
   selector: 'app-navbar-small',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class NavbarSmallComponent {
   show: boolean = false;
+
+  isLogged: boolean = false;
+
+  constructor(private AuthService: AuthService, private Router: Router) {}
+
+  logout() {
+    this.AuthService.logout();
+    this.Router.navigate(['']);
+  }
+
+  ngOnInit() {
+    this.AuthService.logged$.subscribe(
+      (isLogged) => (this.isLogged = isLogged)
+    );
+  }
+
 }
